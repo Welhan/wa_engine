8d796090df57a32f9d721bc8d181699d028410b3
Welhan <welhan.susanto@gmail.com>
2022-12-28 16:41:20 +0700

    memperbaiki login form

--- controller/pagesController.js
+++ controller/pagesController.js
@@ -1,23 +1,20 @@
-const Pages = require("../model/models");
+var Pages = require("../model/models")
 
 module.exports = {
-  index: function (req, res) {
-    res.render("auth/index");
+
+  index: function(req, res) {
+    res.render("auth/index")
   },
-  verify: function (req, res) {
-    res.render("auth/pages/guest/verify");
+  verify: function(req, res) {
+    res.render("auth/pages/guest/verify")
   },
-  blaster: function (req, res) {
-    res.render("auth/pages/guest/blaster");
+  blaster: function(req, res) {
+    res.render("auth/pages/guest/blaster")
   },
-  login: function (req, res) {
-    Pages.getData(req.con, async function (rows) {
-      const servers = await rows;
-      console.log(servers);
-      res.render("auth/pages/loginPage", { data: servers });
-    });
+  login: function(req, res) {
+    Pages.getData(req.con, function(err, rows) {
+      // console.log(rows)
+      res.render("auth/pages/loginPage", { data: rows })
+    })
   },
-  test: function (req, res) {
-    res.render("auth/pages/loginPage");
-  },
-};
+}
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const app = express();
-var methodOverride = require("method-override");
-const path = require("path");
-var con = require("./config/db.js");
+const express = require("express")
+const app = express()
+var methodOverride = require("method-override")
+const path = require("path")
+var con = require("./config/db.js")
 
 // Using pug template engine
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs")
 
 // connecting route to database
-app.use(function (req, res, next) {
-  req.con = con;
-  next();
-});
+app.use(function(req, res, next) {
+  req.con = con
+  next()
+})
 
 // parsing body request
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
 
 // include router
-const pagesRouter = require("./routes/pagesRouter");
+const pagesRouter = require("./routes/pagesRouter")
 
 // routing
-app.use("/", pagesRouter);
+app.use("/", pagesRouter)
 
 // starting server
-app.listen(3000, function () {
-  console.log("server listening on port 3000");
-});
+app.listen(3000, function() {
+  console.log("server listening on port 3000")
+})
